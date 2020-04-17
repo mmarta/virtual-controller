cef0779e0dc054a56b58699d8787fa193da771b8
Marc Marta <ggvicviper@yahoo.com>
2020-04-17 09:47:41 -0400

    Adding all pins/buttons.

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ from flask_cors import CORS
 from gpiozero import LED
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
 CORS(app)
+
+pins = {
+    # Stick
+    'LEFT': LED(18),
+    'UP': LED(23),
+    'RIGHT': LED(24),
+    'DOWN': LED(25),
+    # Action Buttons
+    'B1': LED(17), # Button 1
+    'B2': LED(27),
+    'B3': LED(22),
+    # Start & Coin
+    'COIN': LED(5),
+    'START': LED(6)
+}
+
+for key in pins:
+    pins[key].on()
 
 @app.route('/switch', methods=['POST'])
 def switch():
@@ -12,7 +29,7 @@ def switch():
     pin_num = json_request.get('pin')
     pressed = json_request.get('pressed')
 
-    pin = LED(pin_num)
+    pin = pins[pin_num]
     pin.off() if pressed else pin.on()
 
     return jsonify({
